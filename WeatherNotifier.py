dd557cfff1271a892330d83dec1a9bcbec3bdd71
ppngg <131286838+ppngg@users.noreply.github.com>
2023-11-10 00:38:13 +0700

    Update WeatherNotifier.py

--- WeatherNotifier.py
+++ WeatherNotifier.py
@@ -1,27 +1,8 @@
 import requests
 import time
 from win10toast import ToastNotifier
-
-class WeatherDataFetcher:
-    api_key = None
-    city = None
-    weather_data = None
-    def __init__(self, api_key, city):
-        self.api_key = api_key
-        self.city = city
-        self.api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-
-    def fetch_weather_data(self):
-        try:
-            response = requests.get(self.api_url)
-            if response.status_code == 200:
-                self.weather_data = response.json()
-            else:
-                print(f"Failed to retrieve weather information.")
-        except requests.exceptions.RequestException as e:
-            print(f"Request to OpenWeatherMap API failed: {str(e)}")
-    def get_weather_data(self):
-        return self.weather_data
+from bs4 import BeautifulSoup
+from task1 import WeatherDataFetcher
 
 class WeatherNotification:
     temperature = None
@@ -31,86 +12,39 @@ class WeatherNotification:
     city = None
     country = None
     description = None
+    id = None
     processed_data = {}
 
     def __init__(self, processed_data):
         self.processed_data = processed_data
-        self.description = ['main']['description']
-        self.temperature = processed_data['main']['temp']
-        self.max = processed_data['main']['temp_max']
-        self.min = processed_data['main']['temp_min']
-        self.humidity = processed_data['main']['humidity']
-        self.city = processed_data['name']
-        self.country = processed_data['sys']['country']
+        self.description = processed_data['description']
+        self.temperature = processed_data['temperature']
+        self.max = processed_data['max']
+        self.min = processed_data['min']
+        self.humidity = processed_data['humidity']
+        self.city = processed_data['city']
+        self.country = processed_data['country']
+        self.id = processed_data['id']
 
     def check_bad_weather(self):
         toaster = ToastNotifier()
-        if self.temperature > 32:
-            toaster.show_toast("Weather Alert", f"High temperature in {self.city}, {self.country}",
-                               duration=10,
-                               threaded=True)
+        if (self.temperature > 28 and self.humidity > 50):
+            toaster.show_toast("Weather Alert", f"{self.temperature}°C, {self.humidity}% humidity in {self.city}, {self.country}",
+                                duration=10,
+                                threaded=True)
             while toaster.notification_active():
                 time.sleep(0.005)
-        if self.humidity > 70:
-            toaster.show_toast("Weather Alert", f"High humidity in {self.city}, {self.country}",
-                               duration=10,
-                               threaded=True)
-            while toaster.notification_active():
-                time.sleep(0.005)
+        elif (self.id >= 200 and self.id < 531):
+            toaster.show_toast("Weather Alert", f"{self.description} in {self.city}, {self.country}")
 
-def create_id_description_dict():
-    id_description_dict = {}
-    for i in range(200, 532):
-        if i == 200:
-            id_description_dict[i] = "thunderstorm with light rain"
-        elif i == 201:
-            id_description_dict[i] = "thunderstorm with rain"
-        elif i == 202:
-            id_description_dict[i] = "thunderstorm with heavy rain"
-        elif i == 210:
-            id_description_dict[i] = "light thunderstorm"
-        elif i == 211:
-            id_description_dict[i] = "thunderstorm"
-        elif i == 212:
-            id_description_dict[i] = "heavy thunderstorm"
-        elif i == 221:
-            id_description_dict[i] = "ragged thunderstorm"
-        elif i == 230:
-            id_description_dict[i] = "thunderstorm with light drizzle"
-        elif i == 231:
-            id_description_dict[i] = "thunderstorm with drizzle"
-        elif i == 232:
-            id_description_dict[i] = "thunderstorm with heavy drizzle"
-        elif i == 300:
-            id_description_dict[i] = "light intensity drizzle"
-        elif i == 301:
-            id_description_dict[i] = "drizzle"
-        elif i == 302:
-            id_description_dict[i] = "heavy intensity drizzle"
-        elif i == 310:
-            id_description_dict[i] = "light intensity drizzle rain"
-        elif i == 311:
-            id_description_dict[i] = "drizzle rain"
-        elif i == 312:
-            id_description_dict[i] = "heavy intensity drizzle rain"
-        elif i == 313:
-            id_description_dict[i] = "shower rain and drizzle"
-        elif i == 314:
-            id_description_dict[i] = "heavy shower rain and drizzle"
-        elif i == 321:
-            id_description_dict[i] = "shower drizzle"
-        elif i == 500:
-            id_description_dict[i] = "light rain"
-        elif i == 501:
-            id_description_dict[i] = "moderate rain"
-        elif i == 502:
-            id_description_dict[i] = "heavy intensity rain"
-        elif i == 503:
-            id_description_dict[i] = "very heavy rain"
-        elif i == 504:
-            id_description_dict[i] = "extreme rain"
-        elif i == 511:
-            id_description_dict[i] = "freezing rain"
-        elif i == 520:
-            id_description_dict[i] = "light intensity shower rain"
-        elif i == 521:
+
+if __name__ == "__main__":
+    wd = WeatherDataFetcher('Ho Chi Minh City')
+    wd.fetch_weather_data()
+    data = wd.get_current_data()
+    if(data):
+        print(data)
+    notif = WeatherNotification(data)
+    notif.check_bad_weather()
+
+    
