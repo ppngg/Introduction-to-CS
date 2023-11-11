bdb6a24982cbcfd8e9eb9bace7d5ed04e46a3831
ppngg <131286838+ppngg@users.noreply.github.com>
2023-11-12 06:47:02 +0700

    database

--- task2.py
+++ task2.py
@@ -1,25 +1,72 @@
-import requests
-import time
+import pyodbc
+from task1 import WeatherDataFetcher
 
-class WeatherDataFetcher:
-    api_key = None
+class WeatherDatabase:
+    temperature = None
+    min = None
+    max = None
+    humidity = None
+    speed = None
     city = None
-    weather_data = None
-    def __init__(self, api_key, city):
-        self.api_key = api_key
-        self.city = city
-        self.api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    country = None
+    description = None
+    def __init__(self):
+        print('Init task 2!')
+        self.conn_str = (
+            r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+            r'DBQ=D:\WeatherFetcher\WeatherHistory.accdb'  # Replace with path to database
+        )
+        
+    def InsertQuery(self):
+        print("Running task 2")
+        fetcher = WeatherDataFetcher()
+        self.data = fetcher.get_current_data()
+        if(self.data):
+            # Create a connection and cursor for InsertQuery
+            insert_conn = pyodbc.connect(self.conn_str)
+            insert_cursor = insert_conn.cursor()
 
-    def fetch_weather_data(self):
-        try:
-            response = requests.get(self.api_url)
-            if response.status_code == 200:
-                self.weather_data = response.json()
-            else:
-                print(f"Failed to retrieve weather information.")
-        except requests.exceptions.RequestException as e:
-            print(f"Request to OpenWeatherMap API failed: {str(e)}")
-    def get_weather_data(self):
-        return self.weather_data
-
+            
+            self.temperature = self.data['temperature']
+            self.min = self.data['min']
+            self.max = self.data['max']
+            self.humidity = self.data['humidity']
+            self.speed = self.data['speed']
+            self.city = self.data['city']
+            self.country = self.data['country']
+            self.description = self.data['description']
+            print("Running task 2!")
+            query = """INSERT INTO WeatherHistory (Temperature, Min, Max, Humidity, Speed, City, Country, Description)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+            insert_cursor.execute(query, (self.temperature, self.min, self.max, self.humidity, self.speed, self.city, self.country, self.description 
+                ))
+            insert_conn.commit()
+            insert_cursor.close()
+            insert_conn.close()
+            print("Database Updated!")
     
+    def FetchQuery(self):
+        # Create a connection and cursor for FetchQuery
+        fetch_conn = pyodbc.connect(self.conn_str)
+        fetch_cursor = fetch_conn.cursor()
+        try:
+            query = """SELECT * FROM WeatherHistory"""
+            fetch_cursor.execute(query)
+            records = fetch_cursor.fetchall()
+            return records
+        except Exception as e:
+            print(f"Error fetching records: {str(e)}")
+            return None
+        finally:
+            fetch_cursor.close()
+            fetch_conn.close()
+    
+if __name__ == "__main__":
+    wd = WeatherDataFetcher('Ho Chi Minh City')
+    processed_data = wd.get_current_data()
+    if(processed_data):
+        print(processed_data)
+    test = WeatherDatabase(processed_data)
+    test.InsertQuery()
+    data = test.FetchQuery()
+    print(data)
