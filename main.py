21d1562247609562be62dab230355df9690deb8d
ppngg <131286838+ppngg@users.noreply.github.com>
2023-11-12 06:55:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from scheduler import *
 from task1 import *
 from task2 import *
 from task3 import *
-from task4 import *
-from task5 import *
-
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
@@ -13,12 +10,10 @@ scheduler.SCH_Init()
 task1 = WeatherDataFetcher('Ho Chi Minh City')
 task2 = WeatherDatabase()
 task3 = AdafruitInterface()
-task4 = WeatherNotification()
 
 scheduler.SCH_Add_Task(task1.get_current_data, 1000,1000)
 scheduler.SCH_Add_Task(task2.InsertQuery, 1000,1000)
 scheduler.SCH_Add_Task(task3.publish_to_adafruit, 5000,500)
-scheduler.SCH_Add_Task(task4.check_bad_weather, 5000,10000)
 
 while True:
     scheduler.SCH_Update()
