a402bc60835e7b1771e997d86b8cf8bd2cd94395
ppngg <131286838+ppngg@users.noreply.github.com>
2023-11-29 13:22:33 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import time
 from scheduler import *
 from task2 import *
 from task3 import *
+from task5 import *
 
 scheduler = Scheduler()
 scheduler.SCH_Init()
 
 task2 = WeatherDatabase()
 task3 = AdafruitInterface()
+task5 = EmailSender()
 
-scheduler.SCH_Add_Task(task2.InsertQuery, 1000,3000)
-scheduler.SCH_Add_Task(task3.publish_to_adafruit, 1000,3000)
+scheduler.SCH_Add_Task(task2.InsertQuery, 1000,7000)
+scheduler.SCH_Add_Task(task3.publish_to_adafruit, 1000,7000)
+scheduler.SCH_Add_Task(task5.Alert, 1000, 50000)
 
 while True:
     scheduler.SCH_Update()
